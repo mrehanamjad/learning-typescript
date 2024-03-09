d64d7460b4ece3f7be241f9362d9fe98fd7ed43c
mrehanamjad <rehanamjad814@gmail.com>
2024-03-09 18:07:17 +0500

    add ts day_15 learnings

--- TS day_15.ts
+++ TS day_15.ts
@@ -11,18 +11,18 @@ type Student = {
 const student1: Student = {
   name: "Rehan",
   age: 18,
-  greet: (country):string=>`Welcome My name is ${student1.name}, I am ${student1.age} year old and I am from ${country}`
+  greet: (country): string =>
+    `Welcome My name is ${student1.name}, I am ${student1.age} year old and I am from ${country}`,
 };
 
 const student2: Student = {
   name: "Shang chi",
   age: 18,
-  greet: (country):string=>`Welcome My name is ${student1.name}, I am ${student1.age} year old and I am from ${country}`
+  greet: (country): string =>
+    `Welcome My name is ${student1.name}, I am ${student1.age} year old and I am from ${country}`,
 };
 
-const instruction: (student1: Student) => string = (
-  student1: Student
-): string => {
+const instruction = (student1: Student): string => {
   const { name, age } = student1;
   return `My name is ${name} and i am ${age} year old`;
 };
